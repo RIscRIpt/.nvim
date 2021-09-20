c761cbce581fac509a883663adc16d79d6e06b1c
RIscRIpt <richard@riscript.com>
2021-09-20 23:37:14 +0300

    Update LSP settings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -3,8 +3,12 @@ local coq = require("coq")
 
 lsp.ccls.setup(coq.lsp_ensure_capabilities({
     init_options = {
+        compilationDatabaseDirectory = "build",
         completion = {
             filterAndSort = false,
+        },
+        index = {
+            multiVersion = 1,
         },
     },
 }))
