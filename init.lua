bc6b23a2f00b8b3ab8571c601dda663acabb5d48
RIscRIpt <richard@riscript.com>
2021-09-22 00:14:40 +0300

    Update config for gitsigns

--- init.lua
+++ init.lua
@@ -30,7 +30,7 @@ require("packer").startup(function(use)
         "lewis6991/gitsigns.nvim",
         requires = { { "nvim-lua/plenary.nvim" } },
         config = function()
-            require("gitsigns").setup {
+            require("gitsigns").setup({
                 signs = {
                     add          = { hl = "GitSignsAdd",    text = nil, numhl = "GitSignsAddNr",    linehl = "GitSignsAddLn" },
                     change       = { hl = "GitSignsChange", text = nil, numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
@@ -38,32 +38,44 @@ require("packer").startup(function(use)
                     topdelete    = { hl = "GitSignsDelete", text = nil, numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
                     changedelete = { hl = "GitSignsChange", text = nil, numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
                 },
-                signcolumn = false,
-                numhl      = true,
-                linehl     = false,
-                word_diff  = true,
                 keymaps = {
                     noremap = true,
-                    ["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-                    ["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
-                    ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-                    ["v <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-                    ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-                    ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-                    ["v <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-                    ["n <leader>hR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-                    ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-                    ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-                    ["n <leader>hS"] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-                    ["n <leader>hU"] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
-                    ["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-                    ["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+                    ["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>lua require(\"gitsigns.actions\").next_hunk()<CR>'" },
+                    ["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>lua require(\"gitsigns.actions\").prev_hunk()<CR>'" },
+                    ["n <leader>hs"] = "<cmd>lua require(\"gitsigns\").stage_hunk()<CR>",
+                    ["v <leader>hs"] = "<cmd>lua require(\"gitsigns\").stage_hunk({vim.fn.line(\".\"), vim.fn.line(\"v\")})<CR>",
+                    ["n <leader>hu"] = "<cmd>lua require(\"gitsigns\").undo_stage_hunk()<CR>",
+                    ["n <leader>hr"] = "<cmd>lua require(\"gitsigns\").reset_hunk()<CR>",
+                    ["v <leader>hr"] = "<cmd>lua require(\"gitsigns\").reset_hunk({vim.fn.line(\".\"), vim.fn.line(\"v\")})<CR>",
+                    ["n <leader>hR"] = "<cmd>lua require(\"gitsigns\").reset_buffer()<CR>",
+                    ["n <leader>hp"] = "<cmd>lua require(\"gitsigns\").preview_hunk()<CR>",
+                    ["n <leader>hb"] = "<cmd>lua require(\"gitsigns\").blame_line(true)<CR>",
+                    ["n <leader>hS"] = "<cmd>lua require(\"gitsigns\").stage_buffer()<CR>",
+                    ["n <leader>hU"] = "<cmd>lua require(\"gitsigns\").reset_buffer_index()<CR>",
                 },
                 watch_index = {
                     interval = 1000,
-                    follow_files = true
+                    follow_files = true,
                 },
-                attach_to_untracked = true,
+                sign_priority = 6,
+                numhl      = true,
+                linehl     = false,
+                signcolumn = false,
+                word_diff  = true,
+                diff_opts = {
+                    internal = false, -- does not work on Windows
+                },
+                max_file_length = 32768,
+                preview_config = {
+                    relative = "cursor",
+                    anchor = "NE",
+                    row = -1024,
+                    col = 1024,
+                    style = "minimal",
+                    border = { " " },
+                    noautocmd = true,
+                },
+                update_debounce = 100,
                 current_line_blame = true,
                 current_line_blame_opts = {
                     virt_text = true,
@@ -73,22 +85,7 @@ require("packer").startup(function(use)
                 current_line_blame_formatter_opts = {
                     relative_time = true,
                 },
-                sign_priority = 6,
-                update_debounce = 100,
-                status_formatter = nil,
-                max_file_length = 40000,
-                preview_config = {
-                    border = "single",
-                    style = "minimal",
-                    relative = "cursor",
-                    row = 0,
-                    col = 1
-                },
-                yadm = {
-                    enable = false
-                },
-            }
-
+            })
         end
     }
     use { "tpope/vim-surround" }
