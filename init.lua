65f578d93d06ff4193f057da03522078cba30960
RIscRIpt <richard@riscript.com>
2022-08-01 17:04:18 +0300

    Remove CHADtree

--- init.lua
+++ init.lua
@@ -24,11 +24,6 @@ require("packer").startup(function(use)
         requires = { { "nvim-lua/plenary.nvim" } },
     }
     use {
-        "ms-jpq/chadtree",
-        branch = "chad",
-        run = ":CHADdeps",
-    }
-    use {
         "lewis6991/gitsigns.nvim",
         requires = { { "nvim-lua/plenary.nvim" } },
         config = function()
