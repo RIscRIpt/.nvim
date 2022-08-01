bf580c13c8d146063b8276078f12f1d26b5d7b21
RIscRIpt <richard@riscript.com>
2022-08-01 17:04:18 +0300

    Add telescope-file-browser

--- init.lua
+++ init.lua
@@ -24,6 +24,16 @@ require("packer").startup(function(use)
         requires = { { "nvim-lua/plenary.nvim" } },
     }
     use {
+        "nvim-telescope/telescope-file-browser.nvim",
+        requires = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            require("telescope").load_extension("file_browser")
+        end
+    }
+    use {
         "lewis6991/gitsigns.nvim",
         requires = { { "nvim-lua/plenary.nvim" } },
         config = function()
