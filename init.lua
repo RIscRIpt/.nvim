e75baee961dc3ec0254e87598d8b872405ee1c96
RIscRIpt <richard@riscript.com>
2022-08-22 23:04:46 +0300

    Add vim-diff-utils

--- init.lua
+++ init.lua
@@ -107,6 +107,7 @@ require("packer").startup(function(use)
     use { "tpope/vim-surround" }
     use { "tpope/vim-repeat" }
     use { "tpope/vim-commentary" }
+    use { "arecarn/vim-diff-utils" }
     use { "PeterRincker/vim-argumentative" }
     use { "editorconfig/editorconfig-vim" }
     use { "godlygeek/tabular" }
