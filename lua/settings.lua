57dfb8d6447341c512e662735ccb61732598218a
RIscRIpt <richard@riscript.com>
2023-03-31 14:15:57 +0100

    Add Windows Terminal mapping to toggle background

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,3 +1,6 @@
-vim.keymap.set("", "<F25>", function()
+function toggle_background()
     vim.o.background = vim.o.background == "dark" and "light" or "dark"
-end)
+end
+
+vim.keymap.set("", "<F25>", toggle_background)
+vim.keymap.set("", "<C-F1>", toggle_background)
