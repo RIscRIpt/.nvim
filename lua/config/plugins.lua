72932332f506af1bd7981b162a2c1f1a1e8e5143
RIscRIpt <richard@riscript.com>
2024-09-05 21:30:07 +0300

    Setup obsidian.nvim

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -242,5 +242,22 @@ require("lazy").setup({
         })
       end
     },
+    {
+      "epwalsh/obsidian.nvim",
+      version = "*",
+      lazy = true,
+      ft = "markdown",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      opts = {
+        workspaces = {
+          {
+            name = "personal",
+            path = "~/projects/docs",
+          },
+        },
+      },
+    },
   },
 })
