873fe7eddf797b0eae6a76b2583f41178108f059
RIscRIpt <richard@riscript.com>
2026-01-22 18:04:35 +0200

    Enable vim.diagnostics

--- lua/config/after.lua
+++ lua/config/after.lua
@@ -3,3 +3,11 @@ lsp_config.config("copilot")
 lsp_config.config("ccls")
 lsp_config.config("pyright")
 lsp_config.config("cmake")
+
+vim.diagnostic.config({
+  underline = true,
+  virtual_text = true,
+  virtual_lines = false,
+  signs = true,
+  float = false,
+})
